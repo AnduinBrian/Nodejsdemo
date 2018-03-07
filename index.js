2645addc7858f1a9ecd37304cfabe8ce2b074f8c
AnduinBrian <36282595+AnduinBrian@users.noreply.github.com>
2018-03-07 18:29:59 +0700

    first commit
    OK

---
+++ index.js
@@ -0,0 +1,32 @@
+// khai bao cho sv lang nghe tren port 8000
+var express = require("express"); // khai bao express framework
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views"); // lay layout trong thu muc views
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(8000) // port
+
+//khai bao mang UserOnline de hien thi len
+var UserOnline = [];
+
+//khi co nguoi ket noi, hien ben Server
+io.on("connection", function(socket){
+    console.log("co nguoi ket noi, socket id: " + socket.id);
+    //hien thi len sv khi co nguoi dang ki
+    socket.on("gui_username", function(data){
+    console.log("User: " + data + " da dang ki");
+    UserOnline.push(data);
+    socket.Username = data;
+    io.sockets.emit("dang_ki_thanh_cong", {username:data, id:socket.id});
+  });
+  socket.on("user_gui_msg", function(data){
+    io.sockets.emit("sv_gui_msg", {Username:socket.Username, msg:data});
+  });
+});
+
+//show trang chu khi co nguoi vo
+app.get("/", function(req, res){
+   res.render("trangchu")
+});
